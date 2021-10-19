3fe90cab8531755d6c55b05d36ec03e7f35e7611
daniel sepulveda <danielsupgg@gmail.com>
2021-10-18 20:48:53 -0500

    se creo la rama development para desarrollo, se creo el template de la pagina about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <div class="header">
+            <img src="" alt="" />
+        </div>
+        <main>
+            <h1>TU NUEVO MEJOR AMIGO A UN CLICK</h1>
+            <div class="container-about">
+                <h2>Quienes somos</h2>
+                <div class="container-information">
+                    <h5>
+                        PETHOME es una plataforma virtual que facilita la
+                        adopcion de mascotas en tu ciudad
+                    </h5>
+                    <button>Comienza ahora</button>
+                    <img src="" alt="" />
+                </div>
+
+                <div class="container-information">
+                    <img src="" alt="" />
+                </div>
+                <div class="footer"></div>
+            </div>
+        </main>
+    </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
