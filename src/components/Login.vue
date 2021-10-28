7d0e194e8667905c3da5e1a83a2191f1dac50cab
Daniel Lozada <67969956+dflr10@users.noreply.github.com>
2021-10-28 11:26:33 -0500

    Change content button tag from "Login" to "Log in".

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -12,7 +12,7 @@
                             <div class="form-outline mb-4">
                                 <input type="password" v-model="user.password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Password"/>
                             </div>
-                            <button class="btn btn-primary btn-lg btn-block" type="submit" > Login
+                            <button class="btn btn-primary btn-lg btn-block" type="submit" > Log in
                             </button>
                             <hr class="my-4">
                             <!-- <p class="text-center text-muted mt-5 mb-0">Don't have an account? <a href="http://localhost:8080/user/signup" class="fw-bold text-body"><u>Sign up here</u></a></p> -->
